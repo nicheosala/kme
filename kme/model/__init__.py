165d6d32c09ede9afa0641700f2f21da986ac5cf
Nicolò Sala <48255219+nicheosala@users.noreply.github.com>
2021-12-15 08:29:07 +0100

    kme: KeyIDsKeyIDs class embedded into file key_ids.py

--- kme/model/__init__.py
+++ kme/model/__init__.py
@@ -1,9 +1,8 @@
 """Pay attention: the order of the following imports is not random: it has to be preserved in order to avoid circual
 import errors. """
-from kme.model.model import *
-from kme.model.key import *
-from kme.model.key_container import *
-from kme.model.key_i_ds_key_i_ds import *
-from kme.model.key_i_ds import *
-from kme.model.key_request import *
-from kme.model.status import *
+from kme.model.model import Model
+from kme.model.key import Key
+from kme.model.key_container import KeyContainer
+from kme.model.key_i_ds import KeyIDsKeyIDs, KeyIDs
+from kme.model.key_request import KeyRequest
+from kme.model.status import Status
--- kme/model/key_i_ds.py
+++ kme/model/key_i_ds.py
@@ -1,7 +1,17 @@
 from dataclasses import dataclass
 
 from kme.errors import EmptyValueError
-from kme.model import KeyIDsKeyIDs, Model
+from kme.model import Model
+
+
+@dataclass(frozen=True, slots=True)
+class KeyIDsKeyIDs(Model):
+    key_ID: str
+    key_ID_extension: object | None = None
+
+    def __post_init__(self) -> None:
+        if self.key_ID is None:
+            raise EmptyValueError
 
 
 @dataclass(frozen=True, slots=True)
