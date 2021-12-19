a93e9787266477fd0d112b22183c3281aeb035ff
Nicolò Sala <48255219+nicheosala@users.noreply.github.com>
2021-12-19 14:48:20 +0100

    qcs: first implementation of 'Delete by IDs' and 'Flush keys' done.

--- qcs/qc_interface.py
+++ qcs/qc_interface.py
@@ -1,4 +1,7 @@
 from typing import Final
+from uuid import UUID
+
+from jsons import dumps
 
 from qcs.client import Client
 from qcs.configs import Config
@@ -27,5 +30,30 @@ class QCInterface:
 
         return res
 
-    def delete_blocks(self) -> DeleteResponse:
-        pass  # TODO
+    def delete_blocks(self, ids: tuple[UUID, ...]) -> DeleteResponse:
+        req: Final[Request] = Request(
+            command="Delete by IDs",
+            attribute="",
+            value=dumps(ids)
+        )
+
+        received: Final[str] = self.client.send(req)
+
+        res: Final[DeleteResponse] = DeleteResponse.from_json(received)
+
+        return res
+
+    def flush_blocks(self) -> DeleteResponse:
+        req: Final[Request] = Request(
+            command="Flush keys",
+            attribute="",
+            value=""
+        )
+
+        received: Final[str] = self.client.send(req)
+
+        res: Final[DeleteResponse] = DeleteResponse.from_json(received)
+        # TODO This does not respect the specs. The specs does not clarify
+        #  what to do in this situation.
+
+        return res
