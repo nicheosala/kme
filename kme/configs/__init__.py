ef45409c02eb6f6c08de1b8dfc8971b7a36d0449
Nicolò Sala <48255219+nicheosala@users.noreply.github.com>
2021-12-27 12:03:12 +0100

    logging added to project.

--- kme/configs/__init__.py
+++ kme/configs/__init__.py
@@ -1,3 +1,4 @@
+import logging
 from os import environ
 from typing import Final
 
@@ -9,9 +10,20 @@ def __set_config() -> Base:
     if env == 'prod':
         return Prod()
     elif env == 'test':
+        logging.basicConfig(level=logging.DEBUG)
         return Test()
     else:
+        logging.basicConfig(level=logging.DEBUG)
         return Dev()
 
 
 Config: Final[Base] = __set_config()
+
+
+def __set_logging() -> None:
+    env: str | None = environ.get('env')
+    if env in ('test', 'dev'):
+        logging.basicConfig(level=logging.DEBUG)
+
+
+__set_logging()
