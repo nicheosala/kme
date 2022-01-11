d1479f2bc635c5c3c3b59d1c619412f4411bb1a0
Nicolò Sala <48255219+nicheosala@users.noreply.github.com>
2022-01-11 18:13:38 +0100

    qcs: changing client-server socket behaviour. The simulator in Polimi Labs expects to receive a 4-bytes message containing the size (unsigned int 32) of the request, before receiving the actual request. Similarly, it sends a 4-bytes message containing the size (unsigned int 32) of the response, before sending the actual response. Now all of this is implemented.

--- qcs/client.py
+++ qcs/client.py
@@ -1,4 +1,6 @@
+"""Manage sending requests and receiving responses to/from the quantum channel."""
 from asyncio import open_connection
+from struct import pack, unpack
 from typing import Final
 
 from jsons import dumps
@@ -8,12 +10,20 @@ from qcs.model import Request
 
 
 async def send(request: Request) -> str:
+    """Send a request to the quantum channel and return a response."""
     reader, writer = await open_connection(Config.HOST, Config.PORT)
 
-    writer.write(bytes(dumps(request, indent=4) + "\n", 'utf-8'))
+    message: Final[str] = dumps(request, indent=4) + "\n"
+    len_message: Final[int] = len(message)
+
+    writer.write(pack(">I", len_message))
     await writer.drain()
 
-    received: Final[str] = str(await reader.read(), 'utf-8')
+    writer.write(bytes(message, "utf-8"))
+    await writer.drain()
+
+    len_received: Final[int] = unpack(">I", await reader.read(4))[0]
+    received: Final[str] = str(await reader.read(len_received), "utf-8")
 
     writer.close()
     await writer.wait_closed()
--- qcs/simulator.py
+++ qcs/simulator.py
@@ -1,5 +1,7 @@
+"""A simulator of a quantum channel."""
 import logging
 from socketserver import TCPServer, StreamRequestHandler, ThreadingMixIn
+from struct import unpack, pack
 from threading import Thread
 from typing import Final, Any
 
@@ -11,7 +13,8 @@ from qcs.resolver import resolve
 
 
 class Simulator:
-    """Start a Quantum Channel Simulator
+    """Start a Quantum Channel Simulator.
+
     Usage:
     with Simulator():
         *do things*
@@ -21,17 +24,15 @@ class Simulator:
     """
 
     def __init__(self, host: str = Config.HOST, port: int = Config.PORT):
+        """Initialize the simulator."""
         self.host = host
         self.port = port
         self.server = ThreadedTCPServer(
-            (Config.HOST, Config.PORT),
-            ThreadedTCPRequestHandler
+            (Config.HOST, Config.PORT), ThreadedTCPRequestHandler
         )
 
         def start_server() -> None:
-            self.server.serve_forever(
-                poll_interval=Config.POLL_INTERVAL
-            )
+            self.server.serve_forever(poll_interval=Config.POLL_INTERVAL)
 
         self.server_thread = Thread(
             target=start_server,
@@ -39,13 +40,16 @@ class Simulator:
         )
 
     def __enter__(self) -> None:
+        """Start the simulator."""
         self.start()
 
     def __exit__(self, exc_type: Any, exc_val: Any, exc_tb: Any) -> None:
+        """Stop the simulator."""
         self.stop()
 
     def start(self) -> None:
         """Start the QC Simulator.
+
         If you start the QCS calling .start() directly, then you must
         explicitly stop it with a call to .stop(). For this reason, usage with
         the 'with' statement should be preferred.
@@ -55,6 +59,7 @@ class Simulator:
 
     def stop(self) -> None:
         """Stop the QC Simulator.
+
         If you stop the QCS calling .stop() directly, before you must
         have explicitly started it with a call to .start(). For this reason,
         usage with the 'with' statement should be preferred.
@@ -66,19 +71,30 @@ class Simulator:
 
 
 class ThreadedTCPServer(ThreadingMixIn, TCPServer):
+    """Provide thread anc TCP functionlaities to the simulator."""
+
     allow_reuse_address = True
 
 
 class ThreadedTCPRequestHandler(StreamRequestHandler):
+    """Class for handling TCP requests."""
 
     def handle(self) -> None:
-        data: Final[str] = self.rfile.readline().decode()
+        """Handle a TCP request."""
+        len_data: Final[int] = unpack(">I", self.rfile.read(4))[0]
+        data: Final[str] = self.rfile.read(len_data).decode()
+
         request: Final[Request] = loads(data, Request, strict=True)
 
         response: Final[Response] = resolve(request)
+
+        len_response: Final[int] = len(response.json_string)
+        self.wfile.write(pack(">I", len_response))
         self.wfile.write(bytes(response.json_string, "utf-8"))
 
-        logging.debug(f"""
+        logging.debug(
+            f"""
         Client: {self.client_address}
         Request: {request}
-        Response: {response}""")
+        Response: {response}"""
+        )
